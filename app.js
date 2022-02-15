dde0e451e9746b8cfc630f94f11a1df0354a4ec6
sukabli <roilance7@gmail.com>
2022-02-15 11:07:52 +0800

    test6

--- app.js
+++ app.js
@@ -150,10 +150,8 @@ app.post('/delete', function(req, res){
 });
 
 
-let port = process.env.PORT;
-if (port == null || port == ""){
-  port = 3000;
-}
+let port = process.env.PORT || 3000;
+
 
 
 app.listen(port, function() {
